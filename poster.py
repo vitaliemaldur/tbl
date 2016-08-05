b7aed31afc0064eeac5637fdb46903aca6d4e0aa
Vitalie Maldur <maldur.vitalik@gmail.com>
2016-08-05 12:59:40 +0300

    Refactor poster

--- poster.py
+++ poster.py
@@ -1,26 +1,38 @@
 import asyncio
 import logging
+import os
 from datetime import datetime
 
-from tbl.db import get_next_for_post, update_posts
-from tbl.posters import facebook
+from tbl import db
+from tbl.posters.twitter import TwitterPoster
+
 
 log = logging.getLogger(__name__)
 
+# get facebook credentials
+FB_ACCESS_TOKEN = os.environ.get('FB_ACCESS_TOKEN')
+FB_PAGE_ID = os.environ.get('FB_PAGE_ID')
+
+# get twetter credentials
+TW_CONSUMER_KEY = os.environ.get('TW_CONSUMER_KEY')
+TW_CONSUMER_SECRET = os.environ.get('TW_CONSUMER_SECRET')
+TW_ACCESS_TOKEN = os.environ.get('TW_ACCESS_TOKEN')
+TW_ACCESS_TOKEN_SECRET = os.environ.get('TW_ACCESS_TOKEN_SECRET')
+
+twitter = TwitterPoster(TW_CONSUMER_KEY, TW_CONSUMER_SECRET,
+                        TW_ACCESS_TOKEN, TW_ACCESS_TOKEN_SECRET)
 
 async def main():
-    document = await get_next_for_post()
-
+    document = await db.get_next_for_post()
     link = document['url']
-    print('Next to post is this url:', link)
 
-    # post to social networks
-    # TODO: add other social posters
-    await facebook.post(link)
+    result = await twitter.post(link)
+    log.info("{link} posted: {result}".format(link=link, result=result))
 
     # mark link as used
-    await update_posts(document['_id'], {'posted_at': datetime.utcnow()})
-
+    if result:
+        update_keys = {'posted_at': datetime.utcnow()}
+        await db.update_posts(document['_id'], update_keys)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
