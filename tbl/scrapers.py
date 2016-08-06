d92f3add2f8ab8cddae73a4510c04bab67496303
Vitalie Maldur <maldur.vitalik@gmail.com>
2016-08-06 15:54:04 +0300

    Fix #8: Implement rss parsing

--- tbl/scrapers.py
+++ tbl/scrapers.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup, Comment
 
 class BaseScraper(object):
     url = None
+    rss = False
 
     @classmethod
     async def fetch_page(cls, session):
@@ -12,13 +13,16 @@ class BaseScraper(object):
             headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X \
             10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.\
             103 Safari/537.36'}
+            parser = 'html.parser' if not cls.rss else 'xml'
             async with session.get(cls.url, headers=headers) as response:
                 assert response.status == 200
-                return BeautifulSoup(await response.read(), 'html.parser')
+                return BeautifulSoup(await response.read(), parser)
 
     @classmethod
     async def get_links(cls, session):
         page = await cls.fetch_page(session)
+        if cls.rss:
+            return {item.link.text for item in page.find_all('item')}
         return {a['href'] for a in page.find_all('a', href=True)}
 
 
